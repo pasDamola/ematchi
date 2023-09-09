c53cb1e991a6b6401579a98a1df073814193701b
pasDamola <oyindadavid18@gmail.com>
2023-09-09 08:25:14 +0100

    Game board grid

---
+++ src/routes/Grid.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Sqaure from "./Sqaure.svelte";
+
+</script>
+<div class="grid">
+    {#each Array(16) as _}
+        <Sqaure/>
+    {/each}
+</div>
+
+<style>
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-template-rows: repeat(4, 1fr);
+        grid-gap: 0.5em;
+        height: 100%;
+    }
+</style>
